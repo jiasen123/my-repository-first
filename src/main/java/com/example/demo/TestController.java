b897d181fcc635a30f0d9e815cff3138362c8a6e
jiasen123 <17337137986@163.com>
2019-10-23 00:31:32 +0800

    aaaaa

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello,123456789";
+        return "hello,aaaa";
     }
 }
