3cdd09d1cb5ad56d4d929c4b4f83497bd678e02c
jiasen123 <17337137986@163.com>
2019-10-24 23:07:43 +0800

    改动2

---
+++ src/main/java/com/example/demo/TestControllerTwo.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello() {
+        String s2 = "改动2";
+        return s2;
+    }
+}
